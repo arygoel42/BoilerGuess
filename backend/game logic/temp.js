8f242517b9f28dc06f059940ee79f1d0a409020f
AlexLi9132 <alexli@purdue.edu>
2024-11-28 15:27:19 -0500

    backend functions added

--- backend/game logic/temp.js
+++ backend/game logic/temp.js
@@ -1,7 +1,4 @@
-// make temp parameter with distance
-// have other logic to give bonuses
 // learn about https requests and how node.js works
-// replace mapComponent.jsx with google maps API
 
 export function calculateDistance(actualPoint, userPoint) {
     if (!actualPoint || !selectedPoint) {
@@ -19,3 +16,27 @@ export function calculateDistance(actualPoint, userPoint) {
     console.log(`Distance: ${distanceInKilometers.toFixed(2)} km`);
     return distance;
 }
+
+// distance in km
+export function calculatePoints (distance) {
+    const maxDistance = 50;
+
+    if (distance < 0.1) {// close enough to perfect
+        return 5000;
+    }
+
+    if (distance > maxDistance) {// max distance, or else no points
+        return 0;
+    }
+
+    const score = 5000 * (1 - distance/maxDistance)
+    return Math.round(score); // round score
+}
+
+export function giveBonuses (streak, guessTime){
+    let totalBonus = 0;
+    totalBonus += 100 * streak; // 100 points for each additional answer in a streak
+    totalBonus += Math.max(0, 50 * (guessTime - 7)); // if user guessed within 7 seconds, give them a bonus for 50 points for each second they guessed early 
+    return totalBonus;
+}
+
