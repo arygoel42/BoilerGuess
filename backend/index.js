9723a803d5524b4101b6fd599e54faab5c6712c8
aryangoel <aryangoel574@gmail.com>
2024-12-23 12:51:31 -0500

    adder

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,7 @@ app.use(
       secure: false,
       maxAge: 24 * 60 * 60 * 1000,
       headers: "Set-Cookie",
-      domain: "https://boiler-guess.vercel.app",
+      // domain: "https://boiler-guess.vercel.app",
     },
   })
 );
