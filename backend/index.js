9af97d9264c7509cb1bfa187575e0fdbfa7be70d
aryangoel <aryangoel574@gmail.com>
2024-12-23 22:11:24 -0500

    are

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "https://boiler-guess.vercel.app",
+    origin: "https://boiler-guess-backend.vercel.app",
     credentials: true, // Allow cookies and other credentials
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
